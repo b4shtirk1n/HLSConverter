c864023d7867c146fd8bacc38ce047160922355b
b4shtirk1n <wowan4512@gmail.com>
2025-08-10 01:00:03 +0300

    UPDATE: add commands

--- command.go
+++ command.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"errors"
+	"time"
+
+	"github.com/beevik/guid"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -15,6 +19,7 @@ func (m Model) KeyCommand(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 	case "enter", " ":
 		if !m.Processing && m.SelectedFile != "" {
 			m.Processing = true
+			m.OutPath = guid.NewString()
 			go m.Convert()
 			return m, m.Spinner.Tick
 		}
@@ -24,4 +29,22 @@ func (m Model) KeyCommand(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 
 func (m *Model) ClearErrCommand() {
 	m.Err = nil
+}
+
+func (m Model) ThroeErrorCommand() (tea.Model, tea.Cmd) {
+	m.Err = errors.ErrUnsupported
+	m.Processing = false
+	m.SelectedFile = ""
+	return m, ClearErrorAfter(2 * time.Second)
+}
+
+func (m *Model) OpenDirCommand() {
+	m.Complete = true
+	OpenDir(m.OutPath)
+}
+
+func (m Model) ThickCommand(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	m.Spinner, cmd = m.Spinner.Update(msg)
+	return m, cmd
 }
